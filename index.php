f87965c9a971b09d6fe7a7c38b79531a36f0b3c7
BramKroone <s1188786@student.windesheim.nl>
2023-09-22 09:32:01 +0200

    College 03

--- index.php
+++ index.php
@@ -2,14 +2,20 @@
 require 'functions/tools.php';
 
 date_default_timezone_set("Europe/Amsterdam");
-
+echo date("D-M-Y" ) . "<br>";
+echo date("H:i:s") . "<br>";
 /**
  * Schrijf een functie waarbij de kleur van de pagina
  * bepaald wordt op basis van tijd
 */
-function changeColor() {
 
-}
+    if (date('H' < 20))
+        echo "<body style=background-color:#1f1f1f;>";
+
+    else
+
+       echo "<body style=background-color:#f8f8f8;>";
+
 
 
 
@@ -24,27 +30,27 @@ function changeColor() {
 //    echo gettype($value) . "<br>";
 //
 //}
-//
+
 //$person = date("D-M-Y");
-//
+
 //CharacterCounterType($person);
 
 /**
  * Schrijf een functie waarbij een array random
  * waarde toont vanuit de array
  */
-$array = ["Stephan", "Arie", "Jan", "Polina", "Renee","Wout"];
-
-function ShuffleArray($array) {
-    if (!is_array($array)) {
-        return;
-    } else {
-        shuffle($array);
-        echo $array[0];
-    }
-}
-
-ShuffleArray($array);
+//$array = ["Stephan", "Arie", "Jan", "Polina", "Renee","Wout"];
+//
+//function ShuffleArray($array) {
+//    if (!is_array($array)) {
+//        return;
+//    } else {
+//        shuffle($array);
+//        echo $array[0];
+//    }
+//}
+//
+//ShuffleArray($array);
 
 /**
  * Verplaats alles naar een function directory, en require de function file.
